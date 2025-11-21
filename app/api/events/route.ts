6269028ca15e6e4c175ebd9471716bb0ae03e408
Welbert-Soares <welbertsoares6@gmail.com>
2025-11-21 14:02:56 -0300

    feature - persistindo dados no banco mongo db

---
+++ app/api/events/route.ts
@@ -0,0 +1,91 @@
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+import connectDB from "@/lib/mongodb";
+import Event from "@/database/event.model";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const formData = await req.formData();
+
+    let event;
+
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (e) {
+      return NextResponse.json(
+        { message: "Formato de dados JSON inválido" },
+        { status: 400 }
+      );
+    }
+
+    const file = formData.get("image") as File;
+
+    if (!file)
+      return NextResponse.json(
+        { message: "Arquivo de imagem é obrigatório" },
+        { status: 400 }
+      );
+
+    const tags = JSON.parse(formData.get("tags") as string);
+    const agenda = JSON.parse(formData.get("agenda") as string);
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: "image", folder: "DevEvent" },
+          (error, results) => {
+            if (error) return reject(error);
+
+            resolve(results);
+          }
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
+
+    const createdEvent = await Event.create({
+      ...event,
+      tags: tags,
+      agenda: agenda,
+    });
+
+    return NextResponse.json(
+      { message: "Evento criado com sucesso", event: createdEvent },
+      { status: 201 }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      {
+        message: "Falha ao criar evento",
+        error: e instanceof Error ? e.message : "Unknown",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1 });
+
+    return NextResponse.json(
+      { message: "Eventos buscados com sucesso", events },
+      { status: 200 }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Falha ao buscar eventos", error: e },
+      { status: 500 }
+    );
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import { EventCard } from '@/components/EventCard'
-import { ExploreBtn } from '@/components/ExploreBtn'
+import { cacheLife } from "next/cache";
 
-import events from '@/lib/constants'
+import { EventCard } from "@/components/EventCard";
+import { ExploreBtn } from "@/components/ExploreBtn";
 
-const Home = () => {
+import { IEvent } from "@/database";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Home = async () => {
+  "use cache";
+  cacheLife("hours");
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">
@@ -20,15 +29,17 @@ const Home = () => {
         <h3>Eventos de destaque</h3>
 
         <ul className="events">
-          {events.map((event) => (
-            <li key={event.title}>
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
